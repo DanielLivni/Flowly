a306298e3b1ff0fb48f200957dd6869ddc8f603c
DanielLivni <daniel@livni.xyz>
2026-05-29 16:14:07 +0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/heebo/400.css'
+import '@fontsource/heebo/500.css'
+import '@fontsource/heebo/600.css'
+import '@fontsource/heebo/700.css'
+import '@fontsource/heebo/800.css'
+import '@fontsource/heebo/900.css'
 import './index.css'
 import App from './App.tsx'
 
